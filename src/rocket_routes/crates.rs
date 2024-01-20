b15c09d08ff02e4000a9a5b7f38213a66acbcda5
Charles <charlesEngX@gmail.com>
2024-01-21 03:18:44 +0800

    🔨 refactor: Overhaul database query logic

--- src/rocket_routes/crates.rs
+++ src/rocket_routes/crates.rs
@@ -1,5 +1,4 @@
-use crate::DbConn;
-use crate::repositories;
+use crate::repositories::CrateReposity;
 use crate::models::*;
 
 use rocket::response::status::NoContent;
@@ -7,49 +6,40 @@ use rocket::serde::json::{json, Json, Value};
 use rocket::http::Status;
 use rocket::response::status::Custom;
 
-use super::server_error;
+use rocket_db_pools::Connection;
+use super::{server_error, DbConn};
 
 #[get("/crates")]
-pub async fn get_crates(db: DbConn) -> Result<Value, Custom<Value>> {
-    db.run(|c| {
-        repositories::CrateReposity::find_mutilple(c, 100)
-            .map(|crates| json!(crates) )
-            .map_err(|e| server_error(&e.into()) )
-    }).await
+pub async fn get_crates(mut c: Connection<DbConn>, _user: User) -> Result<Value, Custom<Value>> {
+    CrateReposity::find_mutilple(&mut c, 100).await
+        .map(|crates| json!(crates) )
+        .map_err(|e| server_error(&e.into()) )
 }
 
 #[get("/crates/<id>")]
-pub async fn view_crates(db: DbConn, id: i32) -> Result<Value, Custom<Value>> {
-    db.run(move |c| {
-        repositories::CrateReposity::find(c, id)
-            .map(|a_crate| json!(a_crate) )
-            .map_err(|e| server_error(&e.into()) )
-    }).await
+pub async fn view_crates(mut c: Connection<DbConn>, id: i32) -> Result<Value, Custom<Value>> {
+    CrateReposity::find(&mut c, id).await
+        .map(|a_crate| json!(a_crate) )
+        .map_err(|e| server_error(&e.into()) )
 }
 
 #[post("/crates", format = "json", data = "<new_crate>")]
-pub async fn create_crates(db: DbConn, new_crate: Json<NewCrate>) -> Result<Value, Custom<Value>> {
-    db.run(|c| {
-        repositories::CrateReposity::create(c, new_crate.into_inner())
-            .map(|a_crate| json!(a_crate) )
-            .map_err(|e| server_error(&e.into()) )
-    }).await
+pub async fn create_crates(mut c: Connection<DbConn>, new_crate: Json<NewCrate>) -> Result<Value, Custom<Value>> {
+    CrateReposity::create(&mut c, new_crate.into_inner()).await
+        .map(|a_crate| json!(a_crate) )
+        .map_err(|e| server_error(&e.into()) )
 }
 
 #[put("/crates/<id>", format = "json", data = "<a_crate>")]
-pub async fn update_crates(db: DbConn, id: i32, a_crate: Json<Crate>) -> Result<Value, Custom<Value>> {
-    db.run(move |c| {
-        repositories::CrateReposity::save(c, id, a_crate.into_inner())
-            .map(|a_crate| json!(a_crate) )
-            .map_err(|e| server_error(&e.into()) )
-    }).await
+pub async fn update_crates(mut c: Connection<DbConn>, id: i32, a_crate: Json<Crate>) -> Result<Value, Custom<Value>> {
+    CrateReposity::save(&mut c, id, a_crate.into_inner()).await
+        .map(|a_crate| json!(a_crate) )
+        .map_err(|e| server_error(&e.into()) )
 }
 
 #[delete("/crates/<id>")]
-pub async fn delete_crates(db: DbConn, id: i32) -> Result<NoContent, Custom<Value>> {
-    db.run(move |c| {
-        repositories::CrateReposity::delete(c, id)
-            .map(|_|  NoContent )
-            .map_err(|e| Custom(Status::InternalServerError, json!(e.to_string())) )
-    }).await
+pub async fn delete_crates(mut c: Connection<DbConn>, id: i32) -> Result<NoContent, Custom<Value>> {
+    CrateReposity::delete(&mut c, id).await
+        .map(|_|  NoContent )
+        .map_err(|e| Custom(Status::InternalServerError, json!(e.to_string())) )
 }
--- src/rocket_routes/rustaceans.rs
+++ src/rocket_routes/rustaceans.rs
@@ -1,54 +1,44 @@
-use crate::repositories;
+use crate::repositories::RustaceanRepository;
 use crate::models::*;
-use crate::DbConn;
 
 use rocket::response::status::NoContent;
 use rocket::serde::json::{json, Json, Value};
 use rocket::response::status::Custom;
 
-use super::server_error;
+use rocket_db_pools::Connection;
+use super::{DbConn, server_error};
 
 #[get("/rustaceans")]
-pub async fn get_rustaceans(db: DbConn) -> Result<Value, Custom<Value>> {
-    db.run(|c| {
-        repositories::RustaceanRepository::find_mutilple(c, 100)
-        .map(|rustaceans| json!(rustaceans) )
-        .map_err(|e| server_error(&e.into()) )
-    }).await
+pub async fn get_rustaceans(mut c: Connection<DbConn>) -> Result<Value, Custom<Value>> {
+    RustaceanRepository::find_mutilple(&mut c, 100).await
+            .map(|rustaceans| json!(rustaceans) )
+            .map_err(|e| server_error(&e.into()) )
 }
 
 #[get("/rustaceans/<id>")]
-pub async fn view_rustaceans(db: DbConn, id: i32) -> Result<Value, Custom<Value>> {
-    db.run(move |c| {
-        repositories::RustaceanRepository::find(c, id)
-            .map(|rustacean| json!(rustacean) )
-            .map_err(|e| server_error(&e.into()) )
-    }).await
+pub async fn view_rustaceans(mut c: Connection<DbConn>, id: i32) -> Result<Value, Custom<Value>> {
+    RustaceanRepository::find(&mut c, id).await
+        .map(|rustacean| json!(rustacean) )
+        .map_err(|e| server_error(&e.into()) )
 }
 
 #[post("/rustaceans", format = "json", data = "<new_rustacean>")]
-pub async fn create_rustaceans(db: DbConn, new_rustacean: Json<NewRustacean>) -> Result<Value, Custom<Value>> {
-    db.run(|c| {
-        repositories::RustaceanRepository::create(c, new_rustacean.into_inner())
-            .map(|rustacean| json!(rustacean) )
-            .map_err(|e| server_error(&e.into()) )
-    }).await
+pub async fn create_rustaceans(mut c: Connection<DbConn>, new_rustacean: Json<NewRustacean>) -> Result<Value, Custom<Value>> {
+    RustaceanRepository::create(&mut c, new_rustacean.into_inner()).await
+        .map(|rustacean| json!(rustacean) )
+        .map_err(|e| server_error(&e.into()) )
 }
 
 #[put("/rustaceans/<id>", format = "json", data = "<rustacean>")]
-pub async fn update_rustaceans(db: DbConn, id: i32, rustacean: Json<Rustacean>) -> Result<Value, Custom<Value>> {
-    db.run(move |c| {
-        repositories::RustaceanRepository::save(c, id, rustacean.into_inner())
-            .map(|rustacean| json!(rustacean) )
-            .map_err(|e| server_error(&e.into()) )
-    }).await
+pub async fn update_rustaceans(mut c: Connection<DbConn>, id: i32, rustacean: Json<Rustacean>) -> Result<Value, Custom<Value>> {
+    RustaceanRepository::save(&mut c, id, rustacean.into_inner()).await
+        .map(|rustacean| json!(rustacean) )
+        .map_err(|e| server_error(&e.into()) )
 }
 
 #[delete("/rustaceans/<id>")]
-pub async fn delete_rustaceans(db: DbConn, id: i32) -> Result<NoContent, Custom<Value>> {
-    db.run(move |c| {
-        repositories::RustaceanRepository::delete(c, id)
-            .map(|_|  NoContent )
-            .map_err(|e| server_error(&e.into()) )
-    }).await
+pub async fn delete_rustaceans(mut c: Connection<DbConn>, id: i32) -> Result<NoContent, Custom<Value>> {
+    RustaceanRepository::delete(&mut c, id).await
+        .map(|_|  NoContent )
+        .map_err(|e| server_error(&e.into()) )
 }
