ad1d54f89cae809d116de5602dd9e24acd2f57a6
Charles <charlesEngX@gmail.com>
2024-01-22 05:00:19 +0800

    🛠️ chore: Format code style

--- src/repositories.rs
+++ src/repositories.rs
@@ -1,57 +1,59 @@
-use crate::schema::*;
 use crate::models::*;
+use crate::schema::*;
 
 use diesel::prelude::*;
-use diesel_async::{RunQueryDsl, AsyncPgConnection};
+use diesel_async::{AsyncPgConnection, RunQueryDsl};
 
-pub struct RustaceanRepository { }
+pub struct RustaceanRepository {}
 
 impl RustaceanRepository {
-
-    pub async fn find_mutilple(c: &mut AsyncPgConnection, limit: i64) -> QueryResult<Vec<Rustacean>> {
+    pub async fn find_mutilple(
+        c: &mut AsyncPgConnection,
+        limit: i64,
+    ) -> QueryResult<Vec<Rustacean>> {
         rustaceans::table
-        .limit(limit)
-        .order(rustaceans::id.desc())
-        .load(c)
-        .await
+            .limit(limit)
+            .order(rustaceans::id.desc())
+            .load(c)
+            .await
     }
 
     pub async fn find(c: &mut AsyncPgConnection, id: i32) -> QueryResult<Rustacean> {
-        rustaceans::table
-        .find(id)
-        .get_result::<Rustacean>(c)
-        .await
+        rustaceans::table.find(id).get_result::<Rustacean>(c).await
     }
 
-    pub async fn create(c: &mut AsyncPgConnection, new_rustacean: NewRustacean) -> QueryResult<Rustacean> {
+    pub async fn create(
+        c: &mut AsyncPgConnection,
+        new_rustacean: NewRustacean,
+    ) -> QueryResult<Rustacean> {
         diesel::insert_into(rustaceans::table)
-        .values(&new_rustacean)
-        .get_result::<Rustacean>(c)
-        .await
+            .values(&new_rustacean)
+            .get_result::<Rustacean>(c)
+            .await
     }
 
-    pub async fn save(c: &mut AsyncPgConnection, id: i32, rustacean: Rustacean) -> QueryResult<Rustacean> {
+    pub async fn save(
+        c: &mut AsyncPgConnection,
+        id: i32,
+        rustacean: Rustacean,
+    ) -> QueryResult<Rustacean> {
         diesel::update(rustaceans::table.find(id))
-        .set((
-            rustaceans::email.eq(rustacean.email.to_owned()),
-            rustaceans::name.eq(rustacean.name.to_owned()),
-        ))
-        .get_result::<Rustacean>(c)
-        .await
+            .set((
+                rustaceans::email.eq(rustacean.email.to_owned()),
+                rustaceans::name.eq(rustacean.name.to_owned()),
+            ))
+            .get_result::<Rustacean>(c)
+            .await
     }
 
     pub async fn delete(c: &mut AsyncPgConnection, id: i32) -> QueryResult<usize> {
-        diesel::delete(rustaceans::table.find(id))
-        .execute(c)
-        .await
+        diesel::delete(rustaceans::table.find(id)).execute(c).await
     }
-
 }
 
-pub struct CrateReposity { }
+pub struct CrateReposity {}
 
 impl CrateReposity {
-
     pub async fn find_mutilple(c: &mut AsyncPgConnection, limit: i64) -> QueryResult<Vec<Crate>> {
         crates::table
             .limit(limit)
@@ -61,10 +63,7 @@ impl CrateReposity {
     }
 
     pub async fn find(conn: &mut AsyncPgConnection, id: i32) -> QueryResult<Crate> {
-        crates::table
-            .find(id)
-            .get_result::<Crate>(conn)
-            .await
+        crates::table.find(id).get_result::<Crate>(conn).await
     }
 
     pub async fn create(c: &mut AsyncPgConnection, new_crate: NewCrate) -> QueryResult<Crate> {
@@ -88,27 +87,30 @@ impl CrateReposity {
     }
 
     pub async fn delete(c: &mut AsyncPgConnection, id: i32) -> QueryResult<usize> {
-        diesel::delete(crates::table
-            .find(id))
-            .execute(c)
-            .await
+        diesel::delete(crates::table.find(id)).execute(c).await
     }
-
 }
 
 pub struct UserRepository;
 
 impl UserRepository {
-
     pub async fn find(c: &mut AsyncPgConnection, id: i32) -> QueryResult<User> {
         users::table.find(id).get_result(c).await
     }
 
-    pub async fn find_by_username(c: &mut AsyncPgConnection, username: &String) -> QueryResult<User> {
-        users::table.filter(users::username.eq(username)).get_result(c).await
+    pub async fn find_by_username(
+        c: &mut AsyncPgConnection,
+        username: &String,
+    ) -> QueryResult<User> {
+        users::table
+            .filter(users::username.eq(username))
+            .get_result(c)
+            .await
     }
 
-    pub async fn find_by_roles(c: &mut AsyncPgConnection) -> QueryResult<Vec<(User, Vec<(UserRole, Role)>)>> {
+    pub async fn find_by_roles(
+        c: &mut AsyncPgConnection,
+    ) -> QueryResult<Vec<(User, Vec<(UserRole, Role)>)>> {
         let users = users::table.load::<User>(c).await?;
         let result = users_roles::table
             .inner_join(roles::table)
@@ -118,7 +120,11 @@ impl UserRepository {
         Ok(users.into_iter().zip(result).collect())
     }
 
-    pub async fn create(c: &mut AsyncPgConnection, new_user: NewUser, role_codes: Vec<RoleCode>) -> QueryResult<User> {
+    pub async fn create(
+        c: &mut AsyncPgConnection,
+        new_user: NewUser,
+        role_codes: Vec<RoleCode>,
+    ) -> QueryResult<User> {
         let user = diesel::insert_into(users::table)
             .values(new_user)
             .get_result::<User>(c)
@@ -127,12 +133,21 @@ impl UserRepository {
         for role_code in role_codes {
             let new_user_role = {
                 if let Ok(role) = RoleRepository::find_by_code(c, &role_code).await {
-                    NewUserRole { user_id: user.id, role_id: role.id }
+                    NewUserRole {
+                        user_id: user.id,
+                        role_id: role.id,
+                    }
                 } else {
                     let name = role_code.to_string();
-                    let new_role = NewRole { code: role_code, name };
+                    let new_role = NewRole {
+                        code: role_code,
+                        name,
+                    };
                     let role = RoleRepository::create(c, new_role).await?;
-                    NewUserRole { user_id: user.id, role_id: role.id }
+                    NewUserRole {
+                        user_id: user.id,
+                        role_id: role.id,
+                    }
                 }
             };
 
@@ -147,16 +162,15 @@ impl UserRepository {
 
     pub async fn delete(c: &mut AsyncPgConnection, id: i32) -> QueryResult<usize> {
         diesel::delete(users_roles::table.filter(users_roles::user_id.eq(id)))
-            .execute(c).await?;
+            .execute(c)
+            .await?;
         diesel::delete(users::table.find(id)).execute(c).await
     }
-
 }
 
 pub struct RoleRepository;
 
 impl RoleRepository {
-
     pub async fn find_by_ids(c: &mut AsyncPgConnection, ids: Vec<i32>) -> QueryResult<Vec<Role>> {
         roles::table.filter(roles::id.eq_any(ids)).load(c).await
     }
@@ -166,8 +180,10 @@ impl RoleRepository {
     }
 
     pub async fn find_by_user(c: &mut AsyncPgConnection, user: &User) -> QueryResult<Vec<Role>> {
-        let user_roles = UserRole::belonging_to(&user).get_results::<UserRole>(c).await?;
-        let role_ids = user_roles.iter().map(|ur| ur.role_id ).collect();
+        let user_roles = UserRole::belonging_to(&user)
+            .get_results::<UserRole>(c)
+            .await?;
+        let role_ids = user_roles.iter().map(|ur| ur.role_id).collect();
 
         Self::find_by_ids(c, role_ids).await
     }
@@ -178,5 +194,4 @@ impl RoleRepository {
             .get_result(c)
             .await
     }
-
 }
